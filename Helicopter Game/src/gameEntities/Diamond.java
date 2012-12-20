94ec3c45fd376d0768dc790d03456294387322a9
Tufan Türkaslan <dofeyna@hotmail.com>
2012-12-20 11:58:00 +0200

    Wall class written..

--- Helicopter Game/src/gameEntities/Diamond.java
+++ Helicopter Game/src/gameEntities/Diamond.java
@@ -6,9 +6,7 @@ import java.awt.image.BufferedImage;
 public class Diamond extends GameObject {
 
 	BufferedImage image;
-	
-	//deneme
-	
+
 	AudioClip audio;
 	
 	public Diamond(){	
---
+++ Helicopter Game/src/gameEntities/Wall.java
@@ -0,0 +1,16 @@
+package gameEntities;
+
+import java.applet.AudioClip;
+import java.awt.image.BufferedImage;
+
+public class Wall extends GameObject {
+
+		BufferedImage image;
+
+		AudioClip audio;
+		
+		public Wall(){	
+		
+	}
+
+}
